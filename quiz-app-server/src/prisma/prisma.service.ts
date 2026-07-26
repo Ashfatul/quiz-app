bdc1866b2797a51472548bfb6aab35cc9260ced6
Ashfatul <ashfatul.islam@gmail.com>
2026-07-26 17:08:12 +0600

    fix pg adapter and prisma nesjs service

--- quiz-app-server/src/prisma/prisma.service.ts
+++ quiz-app-server/src/prisma/prisma.service.ts
@@ -1,9 +1,18 @@
-import { Injectable, OnModuleInit } from '@nestjs/common';
-import { PrismaClient } from '../generated/prisma';
+
+import { Injectable } from '@nestjs/common';
+import { PrismaClient } from '../../generated/prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { Pool } from 'pg';
 
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit {
-  async onModuleInit() {
-    await this.$connect();
-  }
+export class PrismaService extends PrismaClient {
+    constructor() {
+        const adapter = new PrismaPg(
+            new Pool({
+                connectionString: process.env.DATABASE_URL,
+            }),
+        );
+
+        super({ adapter });
+    }
 }
