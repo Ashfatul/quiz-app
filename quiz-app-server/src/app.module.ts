2376edf0abe83d0c6b23be513e3737b5f753d5fd
Ashfatul <ashfatul.islam@gmail.com>
2026-07-26 18:28:21 +0600

    start login module

--- quiz-app-server/src/app.module.ts
+++ quiz-app-server/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RegisterModule } from './auth/register/register.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { LoginModule } from './auth/login/login.module';
 
 @Module({
-  imports: [RegisterModule, PrismaModule],
+  imports: [RegisterModule, PrismaModule, LoginModule],
   controllers: [AppController],
   providers: [AppService],
 })
