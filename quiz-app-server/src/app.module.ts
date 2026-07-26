405c4bb0c5f7c232ccb6ab1c491376cf0cd65a28
Ashfatul <ashfatul.islam@gmail.com>
2026-07-26 16:53:29 +0600

    start uisng prisma

--- quiz-app-server/src/app.module.ts
+++ quiz-app-server/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RegisterModule } from './auth/register/register.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [RegisterModule],
+  imports: [RegisterModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
